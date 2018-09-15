66c3922e5365972799b43b11f56c5117a10a13c9
Kyle Blankenbeckler <kyle.j.blankenbeckler@gmail.com>
2018-09-14 22:28:12 -0500

    new movement functions
    add left/right/slow movement functions and started to play around with friction

--- gameControls.js
+++ gameControls.js
@@ -26,12 +26,30 @@ function clear() {
 }
 
 function setupKeyListeners(){
-	 document.addEventListener('keypress', jump);
+	 document.addEventListener('keypress', function (event) {
+	 if (event.key == "w") {jump()}
+	 else if (event.key == "a") {goLeft()}
+	 else if (event.key == "d") {goRight()}
+	 else if (event.key == "s") {brakes()}
+	 });
 }
 
 function jump(){
-	blueBall.stop()
-	blueBall.vy=-3;
+	blueBall.ay=0;
+	blueBall.vy = -3;
+}
+
+function goLeft(){
+	blueBall.vx -= 50;
+}
+
+function goRight(){
+	blueBall.vx += 50;
+}
+
+function brakes(){
+	if (blueBall.vx > 0) {blueBall.vx += -10;}
+	else if (blueBall.vx < 0) {blueBall.vx += 10;}
 }
 
 function initBalls(){
@@ -45,6 +63,7 @@ function ballsLoop(){
 			balls[i].move();
 			balls[i].checkFloorCollision();
 			balls[i].checkWallCollision();
+			balls[i].isOnGround();
 			balls[i].draw();
 			balls[i].logStats();
 		}
@@ -54,34 +73,38 @@ function ball(radius, color, mass, x, y) {
     this.radius = radius;
 	this.color = color;
 	this.m=mass; //mass to use for other calculations
-	this.e=-0.7;
+	this.e=-0.7; //restitution factor
+	this.u=0.5; //kinetic friction
     this.x = x;
     this.y = y; 
-	this.vx = 50;
+	this.vx = 0;
     this.vy = 0; 
 	this.ax = 0;
 	this.ay = 0;
+	this.onground=false;
 	
 	this.move = function() {
 		this.ay = 0;
 		this.ay += this.m * g; 
 		
-		//simple x motion
+		if (this.onground==true){
+		//friction (still figuring this part out)
+			if (this.vx > 0) {this.ax = -this.u * g;}
+			else if (this.vx < 0) {this.ax = this.u * g;}
+		}
+		//Simple integration for the x-direction 
+		this.vx += this.ax*dt;
 		this.x += this.vx*dt;
 		
-		/* Verlet integration for the y-direction */
+		//simple x motion
+		//this.x += this.vx*dt;
+		
+		// Verlet integration for the y-direction
 		this.dy = this.vy * dt + (0.5 * this.ay * dt * dt);
 		this.y += this.dy*100 ;
 		this.new_ay = this.ay / this.m;
 		this.avg_ay = 0.5 * (this.new_ay + this.ay);
 		this.vy += this.avg_ay * dt;
-	}
-	
-	this.stop = function() {
-		this.ax=0;
-		this.ay=0;
-		this.vx=0;
-		this.vy=0;
 	}
 	
 	this.checkWallCollision = function(){
@@ -116,6 +139,12 @@ function ball(radius, color, mass, x, y) {
 		}
 	}
 	
+	this.isOnGround = function(){
+		if (((this.y + this.radius) == canvas.height)&&(this.vy==0)){
+			this.onground=true;}
+		else {this.onground=false;}
+	}
+	
 	this.draw = function(){
 		ctx.beginPath();
 		ctx.arc(this.x,this.y,this.radius,0,2*Math.PI);
@@ -124,8 +153,10 @@ function ball(radius, color, mass, x, y) {
     }
 	this.logStats = function(){
 		ctx.fillStyle=this.color;
-		ctx.fillText(canvas.height - Math.round(this.y) - this.radius,this.x,10);
-		ctx.fillText(Math.round(this.vy)*-1,this.x,20);
+		ctx.fillText("x: " + (Math.round(this.x)),this.x,10);
+		ctx.fillText("y: " + (canvas.height - Math.round(this.y) - this.radius),this.x,20);
+		ctx.fillText("vx: " + (Math.round(this.vx)),this.x,30);
+		ctx.fillText("vy: " + (Math.round(this.vy)*-1),this.x,40);
 	}
 	
 }
